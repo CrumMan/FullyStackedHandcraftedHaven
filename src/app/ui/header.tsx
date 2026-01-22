a556e08db9a5f15159285eb263593d6fa4bec3c0
CrumMan <122419541+CrumMan@users.noreply.github.com>
2026-01-21 18:12:41 -0600

    added a nav file for header so we can build a dynamic header later with login/logout

--- src/app/ui/header.tsx
+++ src/app/ui/header.tsx
@@ -1,9 +1,15 @@
+import { Navigation } from "./nav"
 interface HeaderProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 
 }
 
 export function Header({ className, ...rest }: HeaderProps) {
     return (
-        <p>Header</p>
+        <>
+        <div className="border">
+        <h1>HandCrafted Haven</h1>
+        <Navigation />
+        </div>
+        </>
     );
 }
---
+++ src/app/ui/nav.tsx
@@ -0,0 +1,9 @@
+export function Navigation(){
+    return(
+    <nav className="mt-4 flex gap-6">
+    <a>Home</a>
+    <a>Product Page</a>
+    <a itemID="Login">Login</a>
+    </nav>
+    )
+}
