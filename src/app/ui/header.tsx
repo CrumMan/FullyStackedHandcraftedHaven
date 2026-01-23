e93200a788765e226e46212d8b92f917f256a715
CrumMan <122419541+CrumMan@users.noreply.github.com>
2026-01-22 20:01:32 -0600

    hamburger is finally working

--- src/app/ui/header.tsx
+++ src/app/ui/header.tsx
@@ -1,21 +1,23 @@
 import Link from "next/link";
+//natvigator export so that we can edit that specific document
+import {Navigation} from "@/app/ui/nav"
 
 export function Header() {
   return (
-    <header className="bg-secondary text-white p-4">
-      <div className="max-w-6xl mx-auto flex justify-between items-center">
+    <header className="bg-secondary text-white p-4 {styles.wrapper}">
+      <div className="flex justify-between items-center {styles.logo}">
         <Link href="/" className="text-xl font-bold">
           Handcrafted Haven
         </Link>
-
+        <Navigation />
         {/* hamburger menu - will add functionality later */}
-        <div itemID="Hamburger_Menu">
+        {/* <div itemID="Hamburger_Menu">
         <button className="flex flex-col gap-1.5 p-2">
           <span className="block w-6 h-0.5 bg-white"></span>
           <span className="block w-6 h-0.5 bg-white"></span>
           <span className="block w-6 h-0.5 bg-white"></span>
         </button>
-        </div>
+        </div> */}
       </div>
     </header>
   );
--- src/app/ui/nav.tsx
+++ src/app/ui/nav.tsx
@@ -1,9 +1,26 @@
+"use client"
+
+import { useState } from "react"
+
 export function Navigation(){
+    const [isOpen, setIsOpen] = useState(false)
+
     return(
-    <nav className="mt-4 flex gap-6">
-    <a>Home</a>
-    <a>Product Page</a>
-    <a itemID="Login">Login</a>
+    <>
+    <button onClick={() => setIsOpen(!isOpen)} aria-label="Toggle menu" className="absolute right-4 top-4 w-8 h-8 z-50" > 
+      <span className={`absolute left-1/2 -translate-x-1/2 w-6 h-0.5 bg-white transition-all duration-300 ${isOpen ? "rotate-45 top-1/2" : "top-2"} `} ></span> 
+      <span className={`absolute left-1/2 -translate-x-1/2 w-6 h-0.5 bg-white transition-opacity duration-300 ${isOpen ? "opacity-0 top-1/2" : "top-1/2"} `} ></span> 
+      <span className={`absolute left-1/2 -translate-x-1/2 w-6 h-0.5 bg-white transition-all duration-300 ${isOpen ? "-rotate-45 top-1/2" : "top-6"} `} ></span> </button>
+    {isOpen && (
+    <nav
+  className={`w-full flex gap-8 mt-2 items-center justify-center bg-secondary overflow-hidden transition-all duration-300
+    ${isOpen ? "opacity-100 max-h-20" : "opacity-0 max-h-0"}`}
+>
+    <a className="text-xl text-white hover:text-gray-300 transition-colors" href="/">Home</a>
+    <a className="text-xl text-white hover:text-gray-300 transition-colors" href="/contact_page">Product Page</a>
+    <a className="text-xl text-white hover:text-gray-300 transition-colors" itemID="Login" href="/login">Login</a>
     </nav>
+    )}
+    </>
     )
 }
