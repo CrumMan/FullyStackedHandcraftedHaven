91cb002376649eb9b37670f3592e6582e89c8910
CrumMan <122419541+CrumMan@users.noreply.github.com>
2026-01-30 10:30:02 -0600

    SEED is working with the two databases!

--- src/app/lib/placeholder-data.ts
+++ src/app/lib/placeholder-data.ts
@@ -6,8 +6,21 @@
     role: "Admin",
     email: 'usertest@test.com',
     password: '123456',
-    bio: "I am admin number 1"
+    bio: "I am admin number 1",
+    userPhoto: `@/public/410544b2-4001-4271-9855-fec4b6a6442a.jpg`,
   },
 ];
 
-export {users};
+const products = [
+  {
+  id:'3f2c8e9b-7a4d-4d6c-b9f2-6e1b0d4a8c21',
+  userId:'410544b2-4001-4271-9855-fec4b6a6442a',
+  name:'First Product',
+  price:12.99,
+  quantity:1,
+  description: "The first handcrafted object",
+  productImg: "@/public/3f2c8e9b-7a4d-4d6c-b9f2-6e1b0d4a8c21.jpg",
+  },
+]
+
+export {users, products};
--- src/app/seed/route.ts
+++ src/app/seed/route.ts
@@ -1,9 +1,50 @@
 import bcrypt from 'bcrypt';
 import postgres from 'postgres';
-import { users } from '../lib/placeholder-data'
+import { users, products } from '../lib/placeholder-data'
 const sql = postgres(process.env.POSTGRES_URL!, {ssl: 'require'});
+async function dropSeededProducts() {
+  await sql`DROP TABLE IF EXISTS products`
+}
+async function seedProducts(){
+  await sql`
+CREATE TABLE IF NOT EXISTS products (
+  id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
 
+  userId UUID NOT NULL,
+  name VARCHAR(20) NOT NULL,
+  price NUMERIC(10,2) NOT NULL,
+  quantity INTEGER NOT NULL,
+  description TEXT NOT NULL,
+  productImg TEXT NOT NULL,
 
+  CONSTRAINT fk_user
+    FOREIGN KEY (userId)
+    REFERENCES account(id)
+    ON DELETE CASCADE
+);
+`;
+  const insertedProducts = await Promise.all(
+    products.map(async (product) =>{
+      return sql `
+      INSERT INTO products (id, userId, name, price, quantity, description, productImg)
+      VALUES (${product.id}, ${product.userId}, ${product.name}, ${product.price}, ${product.quantity}, ${product.description}, ${product.productImg})
+      ON CONFLICT (id) DO NOTHING;
+      `
+    })
+  )
+  return insertedProducts;
+}
+
+async function dropSeededAccount(){
+  const text = 'worked';
+  try{
+    await sql  `DROP TABLE IF EXISTS account`
+  }
+  catch(error){console.log(error);
+  }
+  return text;
+
+}
 
 async function seedAccount() {
   await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
@@ -22,7 +63,8 @@ async function seedAccount() {
     email TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL,
     role authorization_type NOT NULL,
-    bio TEXT NULL
+    bio TEXT NULL,
+    userPhoto TEXT NOT NULL
   );
 `;
 
@@ -31,8 +73,8 @@ async function seedAccount() {
     users.map(async (user) => {
       const hashedPassword = await bcrypt.hash(user.password, 10);
       return sql`
-        INSERT INTO account (id, username, name, email, password, role, bio)
-        VALUES (${user.id}, ${user.username}, ${user.name}, ${user.email}, ${hashedPassword}, ${user.role}, ${user.bio})
+        INSERT INTO account (id, username, name, email, password, role, bio, userPhoto)
+        VALUES (${user.id}, ${user.username}, ${user.name}, ${user.email}, ${hashedPassword}, ${user.role}, ${user.bio}, ${user.userPhoto})
         ON CONFLICT (id) DO NOTHING;
       `;
     }),
@@ -42,12 +84,14 @@ async function seedAccount() {
 }
 
 
-
 export async function GET() {
   try {
     console.log("POSTGRES_URL:", process.env.POSTGRES_URL);
     const result = await sql.begin((sql) => [
-      seedAccount(),
+      // dropSeededAccount(),
+      // seedAccount(),
+      // dropSeededProducts(),
+      // seedProducts(),
     ]);
 
     return Response.json({ message: 'Database seeded successfully' });
