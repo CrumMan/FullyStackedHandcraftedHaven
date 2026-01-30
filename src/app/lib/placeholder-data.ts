f7421309e5d252a6e02a9024916f92182bd37c87
CrumMan <122419541+CrumMan@users.noreply.github.com>
2026-01-30 09:17:10 -0600

    seed for account now works

--- src/app/lib/placeholder-data.ts
+++ src/app/lib/placeholder-data.ts
@@ -3,7 +3,7 @@
     id: '410544b2-4001-4271-9855-fec4b6a6442a',
     username: 'userConrol',
     name:'Admin Name',
-    authorization: 'Admin',
+    role: "Admin",
     email: 'usertest@test.com',
     password: '123456',
     bio: "I am admin number 1"
--- src/app/seed/route.ts
+++ src/app/seed/route.ts
@@ -3,26 +3,36 @@ import postgres from 'postgres';
 import { users } from '../lib/placeholder-data'
 const sql = postgres(process.env.POSTGRES_URL!, {ssl: 'require'});
 
-async function seedUsers() {
+
+
+async function seedAccount() {
   await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+  
+  try { await sql` CREATE TYPE authorization_type AS ENUM ('User', 'Creator', 'Admin'); `; } 
+    catch (error) { console.log("ENUM ERROR CODE:", (error as any).code);
+     if ((error as any).code !== '42710'){
+       throw error; }
+  }
+
   await sql`
-    CREATE TABLE IF NOT EXISTS user (
-      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-      username VARCHAR(25) NOT NULL,
-      name VARCHAR(50) NOT NULL,
-      email TEXT NOT NULL UNIQUE,
-      password TEXT NOT NULL,
-      authorization ENUM ('User','Creator','Admin')
-      bio TEXT NULL
-    );
-  `;
+  CREATE TABLE IF NOT EXISTS account (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    username VARCHAR(25) NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    role authorization_type NOT NULL,
+    bio TEXT NULL
+  );
+`;
+
   
   const insertedUsers = await Promise.all(
     users.map(async (user) => {
       const hashedPassword = await bcrypt.hash(user.password, 10);
       return sql`
-        INSERT INTO users (id, username, name, email, password, authorization, bio)
-        VALUES (${user.id}, ${user.username}, ${user.name}, ${user.email}, ${hashedPassword}, ${user.authorization}, ${user.bio})
+        INSERT INTO account (id, username, name, email, password, role, bio)
+        VALUES (${user.id}, ${user.username}, ${user.name}, ${user.email}, ${hashedPassword}, ${user.role}, ${user.bio})
         ON CONFLICT (id) DO NOTHING;
       `;
     }),
@@ -31,14 +41,18 @@ async function seedUsers() {
   return insertedUsers;
 }
 
+
+
 export async function GET() {
   try {
+    console.log("POSTGRES_URL:", process.env.POSTGRES_URL);
     const result = await sql.begin((sql) => [
-      seedUsers(),
+      seedAccount(),
     ]);
 
     return Response.json({ message: 'Database seeded successfully' });
   } catch (error) {
+    console.log(error)
     return Response.json({ error }, { status: 500 });
   }
 }
