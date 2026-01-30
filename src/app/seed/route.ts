ff3eca4356c361bc776cd06dea59a31d4ef08ea2
CrumMan <122419541+CrumMan@users.noreply.github.com>
2026-01-29 20:40:49 -0600

    Fixing database typo

--- src/app/seed/route.ts
+++ src/app/seed/route.ts
@@ -1,7 +1,7 @@
 import bcrypt from 'bcrypt';
 import postgres from 'postgres';
 import { users } from '../lib/placeholder-data'
-const sql = postgres(process.env.POSGRESS_URL!, {ssl: 'require'});
+const sql = postgres(process.env.POSTGRES_URL!, {ssl: 'require'});
 
 async function seedUsers() {
   await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
