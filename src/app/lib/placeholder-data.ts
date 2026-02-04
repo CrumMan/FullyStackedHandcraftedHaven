41b8185b22389f386c505089c3fbcaf3f9e61392
oseimacdonald <oseimacdonald@gmail.com>
2026-02-04 15:46:08 +0000

    check

--- src/app/lib/placeholder-data.ts
+++ src/app/lib/placeholder-data.ts
@@ -7,7 +7,7 @@
     email: 'usertest@test.com',
     password: '123456',
     bio: "I am admin number 1",
-    userPhoto: `@/public/410544b2-4001-4271-9855-fec4b6a6442a.jpg`,
+    userPhoto: `/410544b2-4001-4271-9855-fec4b6a6442a.jpg`,
   },
 ];
 
@@ -19,7 +19,7 @@ const products = [
   price:12.99,
   quantity:1,
   description: "The first handcrafted object",
-  productImg: "@/public/3f2c8e9b-7a4d-4d6c-b9f2-6e1b0d4a8c21.jpg",
+  productImg: "/3f2c8e9b-7a4d-4d6c-b9f2-6e1b0d4a8c21.jpg",
   },
 ]
 
